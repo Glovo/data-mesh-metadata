c9894bf5f2bcf994abe9f8deacdb45060986c1d2
miguel.pimenta <pimenta.mig@gmail.com>
2022-06-06 16:26:32 +0100

    [NO-ISSUE] feat: create schema and example files for product

--- metadata_validator.py
+++ metadata_validator.py
@@ -1,24 +1,35 @@
 from os import listdir
+from os.path import join
 
 from model import ValidationException
 from validator import validate_schema
 
 
-def validate(base_path):
-    all = listdir(base_path)
-
-    if len(all) != 2:
-        raise ValidationException(f"Metadata folder should have one directory and one file, instead {all}")
-
-    domain_file = next(filter(lambda x: ".yml" in x, all), None)
-    result, error_message = validate_schema(f"{base_path}/{domain_file}", "./schema/domain_schema.yml")
-
+def print_results(file_name, result, error_message):
     if result:
-        print("Successful validation")
+        print(f"{file_name} successfully validated!!")
     else:
         print("Validation failed")
         print(error_message)
         exit(1)
+
+def validate(base_path):
+    base_folder_elements = listdir(base_path)
+
+    if len(base_folder_elements) != 2:
+        raise ValidationException(f"Metadata folder should have one directory and one file, instead {base_folder_elements}")
+
+    domain_file = next(filter(lambda x: ".yml" in x, base_folder_elements), None)
+    result, error_message = validate_schema(f"{base_path}/{domain_file}", "./schema/domain_schema.yml")
+
+    print_results(domain_file, result, error_message)
+
+    product_files =  listdir(join(base_path, "products"))
+
+    for product in product_files:
+        result, error_message = validate_schema(f"{base_path}/products/{product}", "./schema/product_schema.yml")
+
+        print_results(product, result, error_message)
 
 
 if __name__ == '__main__':
